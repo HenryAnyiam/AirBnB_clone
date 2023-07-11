146ff0c1f3af9e4500f94f07f07118d93a6980a8
Katekani Madingana <madinganakn@gmail.com>
2023-07-11 12:20:29 -0700

    all public instance unit tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,6 +6,7 @@
 
 import unittest
 from models.base_model import BaseModel
+import datetime
 
 
 class TestBaseModel(unittest.TestCase):
@@ -17,7 +18,26 @@ class TestBaseModel(unittest.TestCase):
 
         bm1 = BaseModel()
         bm2 = BaseModel()
+        self.assertIsNotNone(bm1.id)
         self.assertIsInstance(bm1, BaseModel)
         self.assertTrue(hasattr(bm1, "id"))
         self.assertNotEqual(bm1.id, bm2.id)
         self.assertIsInstance(bm1.id, str)
+
+    def test_create_at_updated_at(self):
+        """ test cases for created_at """
+
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.created_at, datetime.datetime)
+        self.assertIsInstance(bm1.updated_at, datetime.datetime)
+
+    def test_kwargs_init(self):
+        """ test base model with kwargs initialisation """
+
+        date_tomorrow = '2023-08-11T21:12:23'
+        date_iso = datetime.datetime.fromisoformat(date_tomorrow)
+        bm1 = BaseModel(id='123', created_at=date_tomorrow,
+                        random_arg='test')
+        self.assertEqual(bm1.id, '123')
+        self.assertEqual(bm1.created_at, date_iso)
+        self.assertEqual(bm1.random_arg, 'test')
