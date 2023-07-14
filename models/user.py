3d1f23b5e23df4fe0b0ed1471d5fe9bcb94ad558
HenryAnyiam <anyiamhenry7@gmail.com>
2023-07-14 08:25:11 -0700

    Child Classes

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""module containing class User"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """building class User that inherits
+    from class BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        """initialization of class User"""
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
+
+        BaseModel.__init__(self, *args, **kwargs)
