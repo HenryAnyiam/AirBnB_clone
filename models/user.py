2b501c05a491815001f6054c62a38be9e83c6c92
HenryAnyiam <anyiamhenry7@gmail.com>
2023-07-16 03:52:26 -0700

    file test

--- models/user.py
+++ models/user.py
@@ -11,14 +11,5 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         """initialization of class User"""
-        """self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = "" """
-
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
 
         BaseModel.__init__(self, *args, **kwargs)
