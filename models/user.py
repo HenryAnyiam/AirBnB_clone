04f6f85bf70bfb15462728e9e8f83b6414ba6c2d
Katekani Madingana <madinganakn@gmail.com>
2023-07-16 06:00:13 -0700

    added public attributes to user class

--- models/user.py
+++ models/user.py
@@ -9,11 +9,12 @@ class User(BaseModel):
     """building class User that inherits
     from class BaseModel"""
 
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+
     def __init__(self, *args, **kwargs):
         """initialization of class User"""
 
         BaseModel.__init__(self, *args, **kwargs)
-        self.email = ''
-        self.password = ''
-        self.first_name = ''
-        self.last_name = ''
